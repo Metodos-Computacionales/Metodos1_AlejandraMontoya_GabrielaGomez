1eda22fa75f36f5e72bdd18c13302a0b933a7f70
alejandra-montoya <j.montoyap@uniandes.edu.co>
2022-08-23 22:25:55 -0500

    Lissajous Mejorado

--- taller_1/Lissajous.py
+++ taller_1/Lissajous.py
@@ -2,17 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 n_sides = 5
-delta = (np.pi)/2
-n_x = [1,1,1,1,1,2,2,2,2,3,3,3,4,4,5]
-n_y = [1,2,3,4,5,2,3,4,5,3,4,5,4,5,5]
-t = np.linspace(-(np.pi),2*np.pi,300)
-k=1
-for i in range(0,15):
-  x = np.sin(n_x[i] * t + delta)
-  y = np.sin(n_y[i] * t)
-  plt.subplot(5,5,k)
-  k+=1
-  plt.plot(x,y)
-  plt.axis("off")
+def Lissajous():
+    fig = plt.figure()
+    k = 0
+    n_x = [1,2,3,4,5]
+    n_y = [1,2,3,4,5]
+    theta = np.linspace(0, 2*np.pi, 100)
+    for i in range(5):
+        for j in range(5):
+            k += 1
+            if n_x[i] <= n_y[j]:
+                x = np.sin(n_x[i]*theta)
+                y = np.sin(n_y[j]*theta)
+                liss = fig.add_subplot(n_sides,n_sides,k)
+                liss.plot(x,y)
+                plt.axis('off')
+    pass
 
-
+Lissajous()
